2ad3cdc81e017935054eb3d3a2fe97478b7a0bad
drbobdugan <bdugan@stonehill.edu>
2015-10-16 15:43:00 -0400

    Initial Commit

---
+++ NSURLSessionDownloadTaskExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSessionDownloadTaskExample
+//
+//  Created by Bob Dugan on 10/16/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
